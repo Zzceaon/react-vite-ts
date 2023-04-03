4cdff9eac6e9e45619364c72661457ea184173a5
Zzceaon <zzceaon@outlook.com>
2023-04-03 23:56:07 +0800

    project infrastructure

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+	printWidth: 100,
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'es5',
+	jsxSingleQuote: true,
+	bracketSpacing: true,
+	arrowParens: 'avoid',
+}
